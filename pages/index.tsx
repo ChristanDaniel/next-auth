295cf1c635affb548e046c21a05fb395bf48de0b
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-27 13:34:14 -0300

    form criado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
-import type { NextPage } from 'next'
+import { FormEvent, useState } from 'react';
+import styles from '../styles/Home.module.css'
 
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const Home: NextPage = () => {
+  function handleSumit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password
+    }
+
+    console.log(data)
+  }
+
   return (
-    <h1>Hello World</h1>
+    <form onSubmit={handleSumit} className={styles.container}>
+      <input type="email" value={email} onChange={event => setEmail(event.target.value)} />
+      <input type="password" value={password} onChange={event => setPassword(event.target.value)} />
+
+      <button type="submit">Entrar</button>
+    </form>
   )
 }
-
-export default Home
