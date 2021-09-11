00dc0a3a3c1ab4b5ea80bbc026bda1bf3d638f89
ChristianDaniel <christian_daniel04@hotmail.com>
2021-09-11 14:41:21 -0300

    validando autenticação

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,16 +1,23 @@
 import React, { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { api } from "../services/api";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-    const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-        api.get('/me').then(response => console.log(response))
-        .catch(err => console.log(err))
-    }, [])
+  useEffect(() => {
+    api
+      .get("/me")
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <h1>dashboard: {user?.email}</h1>
-    )
+  return <h1>dashboard: {user?.email}</h1>;
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  return {
+    props: {},
+  };
+});
